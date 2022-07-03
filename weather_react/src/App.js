70be3630be2c3d6fbebcb4851dd013fe5f200c93
Peter Didevich <pdidevich@gmail.com>
2022-07-03 10:45:28 +0300

    format text

--- weather_react/src/App.js
+++ weather_react/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 function App() {
@@ -7,17 +7,17 @@ function App() {
   const [isPopupVisible, setIsPopupVisible] = useState(false)
   const [cities, setCities] = useState([])
   const [city, setCity] = useState({
-        name: 'Moscow',
-        country: 'Russia',
-        countryCode: 'RU',
-     })
+    name: 'Moscow',
+    country: 'Russia',
+    countryCode: 'RU',
+  })
   const [isCitiesLoading, setIsCitiesLoading] = useState(true)
 
-  const setStartedPlace = async function() {
+  const setStartedPlace = async function () {
     let startedCity = {
-        name: localStorage.getItem('city'),
-        country: localStorage.getItem('country'),
-        countryCode: localStorage.getItem('countryCode'),
+      name: localStorage.getItem('city'),
+      country: localStorage.getItem('country'),
+      countryCode: localStorage.getItem('countryCode'),
     }
     if (!startedCity.name || !startedCity.country || !startedCity.countryCode) {
       startedCity = await getPlaceByIp()
@@ -31,43 +31,45 @@ function App() {
 
     setCity(startedCity)
   }
-  const getPlaceByIp = async function() {
+  const getPlaceByIp = async function () {
     try {
-        const response = await axios.get('http://ip-api.com/json/')
-        return {name: response.data?.city, country: response.data?.country, countryCode: response.data?.countryCode}
-      } catch (e) {
-        //TODO error component
-        console.error(e)
-      }
+      const response = await axios.get('http://ip-api.com/json/')
+      return { name: response.data?.city, country: response.data?.country, countryCode: response.data?.countryCode }
+    } catch (e) {
+      //TODO error component
+      console.error(e)
+    }
   }
-  const getWeather = async function() {
+  const getWeather = async function () {
     try {
-       const response = await axios.get("https://api.openweathermap.org/data/2.5/weather", { params: {
-            q: `${city.name},${city.country}`,
-            appid: "f07469a72d6c0229d76ce7b76a2ed11e",
-            units: "metric",
-            lang: 'ru',
-        } });
+      const response = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+        params: {
+          q: `${city.name},${city.country}`,
+          appid: "f07469a72d6c0229d76ce7b76a2ed11e",
+          units: "metric",
+          lang: 'ru',
+        }
+      });
       setTempMax(response.data?.main?.temp_max);
       setTempMin(response.data?.main?.temp_min);
-    } catch(error) {
+    } catch (error) {
       //TODO error component
       console.error(error)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchMyApp() {
       await setStartedPlace()
       getWeather()
     }
     fetchMyApp()
   }, [])
-   
+
   return (
     <div className="App">
       <div>Ваш город "{city.name}, {city.country}"</div>
-        <div>Температура сегодня: {tempMin} - {tempMax} °C</div>
+      <div>Температура сегодня: {tempMin} - {tempMax} °C</div>
       <button onClick="showCityPopup">Выбрать другой город {city.country}</button>
     </div>
   );
